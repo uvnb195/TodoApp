ce8ec8f5208cc59be4f8fabd432550ae15b3e91b
Quan <uvnb195@gmail.com>
2023-11-27 16:03:44 +0700

    Finish Module for Hilt

--- app/src/main/java/com/uvnb195/todoapp/di/AppModule.kt
+++ app/src/main/java/com/uvnb195/todoapp/di/AppModule.kt
@@ -1,8 +1,34 @@
 package com.uvnb195.todoapp.di
 
+import android.app.Application
+import androidx.room.Room
+import com.uvnb195.todoapp.data.TodoDatabase
+import com.uvnb195.todoapp.data.TodoRepository
+import com.uvnb195.todoapp.data.TodoRepositoryImpl
 import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideTodoDatabase(app: Application): TodoDatabase {
+        return Room.databaseBuilder(
+            app,
+            TodoDatabase::class.java,
+            "todo_db"
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTodoRepository(db: TodoDatabase): TodoRepository {
+        return TodoRepositoryImpl(db.dao)
+    }
 
 }
