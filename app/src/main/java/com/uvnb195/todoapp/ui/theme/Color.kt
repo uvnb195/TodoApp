9bf238184b4ed1c87b4399f152319400aa2648a8
Quan <uvnb195@gmail.com>
2023-11-27 15:53:47 +0700

    Setting some Theme.kt

--- app/src/main/java/com/uvnb195/todoapp/ui/theme/Color.kt
+++ app/src/main/java/com/uvnb195/todoapp/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val SkyBlue = Color(0xff0084b4)
+val RedVariant = Color(0xffe15457)
--- app/src/main/java/com/uvnb195/todoapp/ui/theme/Type.kt
+++ app/src/main/java/com/uvnb195/todoapp/ui/theme/Type.kt
@@ -2,27 +2,49 @@ package com.uvnb195.todoapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.uvnb195.todoapp.R
+
+
+val Comfortaa = FontFamily(
+    Font(R.font.comfortaa, FontWeight.Light),
+    Font(R.font.comfortaa, FontWeight.Medium),
+    Font(R.font.comfortaa, FontWeight.Bold),
+    Font(R.font.comfortaa, FontWeight.Normal),
+
+    )
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    bodyMedium = TextStyle(
+        fontFamily = Comfortaa,
+        fontWeight = FontWeight.Light,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
+    bodySmall = TextStyle(
+        fontFamily = Comfortaa,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = Comfortaa,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Comfortaa,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp
+    ),
+
+
+    /* Other default text styles to override
+
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
